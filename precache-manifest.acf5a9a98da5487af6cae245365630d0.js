a5e8789468633341c8a3330038196c4160124a95
André Voronhuk <andrevoronhuk@gmail.com>
2020-08-02 22:38:45 -0300

    commit

---
+++ precache-manifest.acf5a9a98da5487af6cae245365630d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6133622cc1c5c231a49f0572cddc8b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53aa7c9f05e227c224f2",
+    "url": "/static/js/2.ee5f9195.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ee5f9195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1941da29a9ce412f269",
+    "url": "/static/js/main.8165395f.chunk.js"
+  },
+  {
+    "revision": "4dcc8a4a171548826323",
+    "url": "/static/js/runtime-main.dccd1106.js"
+  }
+]);
